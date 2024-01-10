a7226f0360ac1e0605b1e4cf2e6014345febab0f
mattofficeuk <mattofficeuk@googlemail.com>
2024-01-10 17:26:22 +0000

    Improved weather app

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -5,13 +5,16 @@ const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
 const API_KEY = "5ee589c4b11f920ac46bdb0bb2c70320"; // API key for OpenWeatherMap API
+const MAP_API_KEY = "AIzaSyBnWikKwHMeq0iUgU3YjhRXTNpmFJoFYKs"  // API key for Google maps
 
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card
+        // Note: Subtracting zero means that "-0" will become "0", which looks nicer
+        console.log(weatherItem)
         return `<div class="details">
-                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-                    <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
-                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h2>${cityName} (${dayOfWeek(weatherItem.dt_txt.split(" ")[0])})</h2>
+                    <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(0) - 0}°C</h6>
+                    <h6>Wind: ${(weatherItem.wind.speed).toFixed(0)} m/s</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
                 </div>
                 <div class="icon">
@@ -19,14 +22,40 @@ const createWeatherCard = (cityName, weatherItem, index) => {
                     <h6>${weatherItem.weather[0].description}</h6>
                 </div>`;
     } else { // HTML for the other five day forecast card
-        return `<li class="card">
-                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+        return `<li class="card visible">
+                    <h3>${dayOfWeek(weatherItem.dt_txt.split(" ")[0])}</h3>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
-                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(0) - 0}°C</h6>
+                    <h6>Wind: ${(weatherItem.wind.speed).toFixed(0)} m/s</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
                 </li>`;
     }
+}
+
+function dayOfWeek(stringDate) {
+    const dayAsString = new Date(stringDate).toLocaleString('en-us', {  weekday: 'long' })
+    return dayAsString
+}
+
+// const getMapImg = (latitude, longitude) => {
+//     const latlon = `${latitude},${longitude}`
+//     const MAP_API_URL = `https://maps.googleapis.com/maps/api/staticmap?center=${latlon}&zoom=10&size=550x550&sensor=false&key=${MAP_API_KEY}&callback=myMap`;
+//     console.log(MAP_API_URL)
+// }
+
+// function myMap(latitude, longitude) {
+//     var mapProp= {
+//       center:new google.maps.LatLng(latitude, longitude),
+//       zoom:5,
+//     };
+//     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+// }
+async function getMapImg(latitude, longitude) {
+    const latlon = latitude + "," + longitude
+    const MAP_API_URL = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=10&size=550x550&sensor=false&key=" + MAP_API_KEY;
+    const map = document.querySelector(".map")
+    console.log(MAP_API_URL)
+    map.setAttribute("src", MAP_API_URL)
 }
 
 const getWeatherDetails = (cityName, latitude, longitude) => {
@@ -73,6 +102,7 @@ const getCityCoordinates = () => {
         if (!data.length) return alert(`No coordinates found for ${cityName}`);
         const { lat, lon, name } = data[0];
         getWeatherDetails(name, lat, lon);
+        getMapImg(lat, lon);
     }).catch(() => {
         alert("An error occurred while fetching the coordinates!");
     });
@@ -87,15 +117,16 @@ const getUserCoordinates = () => {
             fetch(API_URL).then(response => response.json()).then(data => {
                 const { name } = data[0];
                 getWeatherDetails(name, latitude, longitude);
+                getMapImg(latitude, longitude);
             }).catch(() => {
                 alert("An error occurred while fetching the city name!");
             });
         },
         error => { // Show alert if user denied the location permission
             if (error.code === error.PERMISSION_DENIED) {
-                alert("Geolocation request denied. Please reset location permission to grant access again.");
+                alert("Geolocation request denied. Please reset location permissions or type a location in the search bar.");
             } else {
-                alert("Geolocation request error. Please reset location permission.");
+                alert("Geolocation request error. Please reset location permissions or type a location in the search bar.");
             }
         });
 }
